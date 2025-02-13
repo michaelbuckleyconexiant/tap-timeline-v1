9b366f13da099e38863f73f3f030af7f2fef8a90
michaelbuckleyconexiant <michael.buckley@conexiant.com>
2025-02-13 15:06:36 -0500

    Add files via upload

---
+++ js/animation-fade.js
@@ -0,0 +1,17 @@
+//fade in animation 
+
+document.addEventListener("DOMContentLoaded", function () {
+    const elements = document.querySelectorAll(".fade-in-left, .fade-in-right");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            }
+        });
+    }, { threshold: 0.3 });
+
+    elements.forEach(element => {
+        observer.observe(element);
+    });
+});
